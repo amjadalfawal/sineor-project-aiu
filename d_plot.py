e427d59ca4a3fd1ec4aa6308b96bfa9a3d0c8514
amjadalfawal <aamghed@gmail.com>
2019-12-25 18:19:20 +0200

    simple protien 3d drawer

--- d_plot.py
+++ d_plot.py
@@ -7,25 +7,43 @@ from drawnow import drawnow
 
 def plot_3d(teritry):
     print('test')
-    
-    for i in range(0,len(teritry[0])): 
-        col1 = [val[i] for val in teritry]
-    
-
+    x = []
+    y = []
+    z = []
     fig = plt.figure()
+    m = '.'
     ax = plt.axes(projection='3d')
+    for i in range(0,len(teritry[0])):
+        col1 = [val[i] for val in teritry]
 
-    # Data for a three-dimensional line
-    zline = np.linspace(0, 15, 1000)
-    xline = np.sin(zline)
-    yline = np.cos(zline)
-    ax.plot3D(xline, yline, zline, 'gray')
+        if col1[0] == 0 and col1[1] == 0 and col1[2] == 0 :
+            continue
 
-    # Data for three-dimensional scattered points
-    zdata = 15 * np.random.random(100)
-    xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
-    ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
-    ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
+        # fig = plt.figure()
+        # ax = plt.axes(projection='3d')
+        xs = col1[0]
+        ys = col1[1]
+        zs = col1[2]
+        ax.scatter(xs, ys, zs , marker=m)
+        # Data for a three-dimensional line
+        x.append(col1[0])
+        y.append(col1[1])
+        z.append(col1[2])
+
+
+     
+        # ax.plot3D(int(col1[0]), int(col1[1]), int(col1[2]), 'gray')
+        # Data for three-dimensional scattered points
+        # zdata = 15 * np.random.random(100)
+        # xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
+        # ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
+        # ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
+    
+
+    ax.plot3D(x, y, z, 'blue')
+    # zdata = 15 * np.random.random(100)
+    # ax.scatter3D(x, y, z, c=np.squeeze(zdata), cmap='gray')
+
     plt.show()
 
 
